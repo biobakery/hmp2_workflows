64a1752192b60be4ea346f204f31a7aad214b729
Human Microbiome Project phase 2 portal <ibdmdb@ibdmdb-int2.broadinstitute.org>
2015-02-19 15:43:21 -0500

    Add HMP2 filesystem polling scripts

---
+++ poll/hmp2_poll.sh
@@ -0,0 +1,108 @@
+#!/bin/sh
+##
+## Script searches given filesystem(s) for any new or updated file
+## in the time range of [since the last time the script ran - to -
+## 5 minutes ago].  If file(s) are found, it uses curl to notify
+## jenkins giving it the containing directory
+## as a parameter.
+##
+## Note: The time range attempts to prevent files that are still
+## being uploaded from being processed before they are fully
+## loaded onto the filesystem.
+##
+
+# Settable Parameters
+TIME_DELAY_SEC=900 # 15 min
+TIME_DELAY_MIN=15 # 15 min
+DATA_DIR=/seq/ibdmdb/data_deposition
+ANADAMA_DIR=/seq/ibdmdb/centos6
+TMP_DIR=/seq/ibdmdb/centos6/tmp
+
+##
+
+function call_anadama {
+
+  #echo "mibc_build dag --project='$1' -a --tmpfiledir=${ANADAMA_DIR}/tmp/tm | mibc_tm -t lsf -g 10"
+  echo "mibc_run.sh $1 lsf 10"
+  mibc_run.sh $1 lsf 10
+  mail -s "mibc_run.sh $1 called... " kbayer@broadinstitute.org <<EOF
+no body.
+EOF
+
+  mibc_run.sh $1 lsf 10 &
+  #mibc_build dag --project="$1" -a --tmpfiledir=${ANADAMA_DIR}/tmp/tm | mibc_tm -t lsf -g 10
+
+}
+
+function init_timestamps {
+
+  # only create initial timestamp if it doesn't exist
+  if [ ! -f ${DATA_DIR}/.timestamp.empty ]; then
+    echo "creating .timestamp.empty"
+    touch ${DATA_DIR}/.timestamp.empty
+  fi
+  # uptime timestamp prior to processing so we don't loose any new events
+  # during processing itself - we need them all.
+  touch -d `date --date="-${TIME_DELAY_SEC} seconds" +%H:%M:%S` ${DATA_DIR}/.next_timestamp.empty
+
+}
+
+function finalize_timestamps {
+  mv ${DATA_DIR}/.next_timestamp.empty ${DATA_DIR}/.timestamp.empty
+}
+
+# Source project specific configuration parameters
+if [ -f ${ANADAMA_DIR}/bin/activate ]; then
+  . ${ANADAMA_DIR}/bin/activate
+else
+  echo "${ANADAMA_DIR}/bin/activate file not found"
+  exit -1
+fi
+
+# Are we still running (from the last time?) if so die now
+if [ -f ${DATA_DIR}/.next_timestamp.empty ]; then
+  echo "Still running last poll; exiting now"
+  exit 0
+fi
+
+init_timestamps
+
+# We want all files newer than the last run of this script AND older than TIME_DELAY minutes ago
+# (to prevent processing the file while it is still in-flight).
+#
+# After we get that set of files, strip out any that match our DATA_FIND_GREP terms
+# Then run each file through dirname to get its directory
+# then sort and find only the unique directory entries
+
+files=`find ${DATA_DIR} \
+       -name mibc_products -prune -o \
+       -newer ${DATA_DIR}/.timestamp.empty \
+       -and ! -newer ${DATA_DIR}/.next_timestamp.empty \
+       -type f \
+       -print 2>/dev/null | grep -v -e '\.log$' -e '\.empty'`
+
+for file in ${files}
+do
+  if [[ ${file} == "" ]]; then
+    continue
+  fi
+  echo "file: ${file}"
+  dir=`dirname "${file}"`
+  directories=`echo "${directories}" && echo "${dir}"`
+done
+
+directories=`echo "${directories}" | sort | uniq`
+
+# loop through all directories with new content that have come through quiet period...
+for dir in ${directories}
+do
+  echo "testing ${dir}"
+  if test "`find ${dir} -maxdepth 1 -type d -mmin +${TIME_DELAY_MIN}`"; then
+    call_anadama "${dir}"
+  else
+    echo "dir: ${dir} is newer than ${TIME_DELAY} seconds - skipping"
+  fi
+done
+
+finalize_timestamps
+
---
+++ poll/mibc_run.sh
@@ -0,0 +1,122 @@
+#!/bin/sh
+##
+## HMP2 Broad specific script deployed runs both mibc_build and mibc_tm with sane defaults
+## 
+##
+
+broad_data=/seq/ibdmdb/centos6/tmp/hmp2_workflows/broad_internal_data/link_data.py
+
+function mk_map {
+
+  if [ -f "${InputFile}/map.txt" ]; then
+    echo "map.txt file present."
+  else
+    files=`cd "${InputFile}" && ls | grep -v -e \.log -e metadata -e map.txt -e mibc_products | awk -F_ '{print $1}'`
+    cat > "${InputFile}/map.txt" << EOF
+#SampleID
+${files}
+EOF
+  fi
+}
+
+function mk_metadata {
+
+  if [ -f "${InputFile}/metadata.txt" ]; then
+    echo "metadata.txt file present."
+  else
+    if [ $1 == "WGS" ]; then
+      sixteenS="false"
+    else
+      sixteenS="true"
+    fi
+    files=`cd "${InputFile}" && ls | grep -v -e \.log -e map.txt -e mibc_products | xargs | tr ' ' '	' | sed 's/ $//'`
+    cat > "${InputFile}/metadata.txt" << EOF
+pi_first_name	Curtis
+pi_last_name	Huttenhower
+pi_contact_email	chuttenh@hsph.harvard.edu
+lab_name	Huttenhower
+researcher_first_name	data
+researcher_last_name	deposition
+researcher_contact_email	kbayer@hsph.harvard.edu
+study_title	HMP2
+study_description	16S description
+sample_type	microbial
+filename	${files}
+collection_start_date	2013-01-01
+collection_end_date	2013-01-01
+submit_to_insdc	false
+16s_data	$sixteenS
+reverse_primer	CCGTCAATTCMTTTRAGT
+platform	454
+visualize	true
+EOF
+  fi
+}
+
+function mk_broaddata {
+ 
+  spreadsheet=`ls ${InputFile}/*.xlsx 2>/dev/null | sed -n '1p'` 
+  if [ -f "${spreadsheet}" ]; then
+    echo "processing ${spreadsheet}"
+    anadama run -f "${broad_data}" s="${spreadsheet}"
+    return 1
+  else
+    return 0
+  fi
+}
+
+function call_mibc 
+{
+  cd /seq/ibdmdb/centos6/tmp/tm
+  echo "mibc_build dag --project='${InputFile}' -a --tmpfiledir=/seq/ibdmdb/centos6/tmp/tm | mibc_tm -t ${Type} -g ${Governor} -p 8888"
+  mibc_build dag --project="${InputFile}" -a --tmpfiledir=/seq/ibdmdb/centos6/tmp/tm | mibc_tm -t ${Type} -g ${Governor} -p 8888
+}
+
+if [ $# -ne 3 ]; then
+  echo "run_mibc.sh directory [local|lsf] governor"
+  exit -1
+else
+  InputFile=$1
+  Type=$2
+  Governor=$3
+fi
+
+if [ -z "${InputFile}" ]; then
+  echo "Error: InputFile env variable empty."
+  exit -1
+fi
+
+
+data=`echo "${InputFile}" | awk -F'/' '{print $6}'`
+
+if [ -z $data ]; then
+  echo "Error: data variable empty."
+  echo "InputDir: ${InputDir}"
+  exit -1
+fi
+
+echo "data: $data"
+
+case $data in
+
+  WGS)
+    mk_broaddata 
+    if [ $? == 0 ]; then
+      mk_map
+      mk_metadata $data
+      call_mibc
+    fi
+    ;;
+  16S)
+    mk_broaddata 
+    if [ $? == 0 ]; then
+      mk_metadata $data
+      call_mibc
+    fi
+    ;;
+  *)
+    echo "Unknown data: $data in InputDir: ${InputDir}"
+    echo "To support this new datatype, update script new_data.sh"
+    exit -1
+    ;;
+esac
