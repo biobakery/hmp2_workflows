750555a35570de1de541acfb878c31737f54321a
schwager-hsph <schwager@hsph.harvard.edu>
2016-01-28 10:23:53 -0500

    feature: add wgs and wms submission code

--- hmp2/dcc/__init__.py
+++ hmp2/dcc/__init__.py
@@ -9,12 +9,14 @@ from . import study
 from . import subject
 from . import visit
 from . import sixteen
-
+from . import wgs
+from . import wts
 
 class settings:
     username = 'rschwager'
     
 def submit(hosp_joined_fname, sixt_joined_fname, sixt_fnames,
+           wgs_joined_fname, wgs_fnames, wts_joined_fname, wts_fnames,
            dcc_user, dcc_pass):
     logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s")
     logging.getLogger().setLevel(logging.DEBUG)
@@ -34,6 +36,25 @@ def submit(hosp_joined_fname, sixt_joined_fname, sixt_fnames,
     samples = list()
     for grp in chain.from_iterable(visits):
         samples.extend([s for s in islice(grp, 1, None)])
+
     ps_groups = map(
        list, sixteen.from_file(sixt_joined_fname, samples, sixt_fnames))
     sixteen.save_all(ps_groups)
+
+    ps_groups = map(
+       list, sixteen.from_file(
+           wgs_joined_fname, samples, wgs_fnames,
+           sample_id_idx=lambda r: "{}-{}".format(r[1][1:3], r[1][3:]),
+           fname_idx=1,
+           parse_record=wgs.parse_record)
+    )
+    sixteen.save_all(ps_groups)
+
+    ps_groups = map(
+       list, sixteen.from_file(
+           wts_joined_fname, samples, wts_fnames, sample_id_idx=0, fname_idx=1,
+           parse_record=wts.parse_record)
+    )
+    sixteen.save_all(ps_groups)
+
+    
