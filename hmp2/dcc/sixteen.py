761b1fe951485d631026e03fb520edd19f437686
schwager-hsph <schwager@hsph.harvard.edu>
2016-01-28 10:24:26 -0500

    fix: make sequence submission code idempotent

--- hmp2/dcc/sixteen.py
+++ hmp2/dcc/sixteen.py
@@ -1,13 +1,18 @@
 import os
 import sys
 import hashlib
-from operator import attrgetter
 import multiprocessing
+from itertools import islice
+from operator import attrgetter
+from operator import itemgetter
 
 import cutlass
 import cutlass.mimarks
 from toolz import groupby, compose, first
+from toolz import countby
+from toolz import second
 import anadama.util
+from Bio import SeqIO
 
 from .subject import fields
 from .visit import SaveError
@@ -31,7 +36,11 @@ class settings:
     
 
 def default_prep(sample):
-    prep = cutlass.SixteenSDnaPrep()
+    maybe_extant_preps = list(sample.sixteenSDnaPreps())
+    if maybe_extant_preps:
+        prep = maybe_extant_preps[0]
+    else:
+        prep = cutlass.SixteenSDnaPrep()
     prep.comment = "Broad IBDMDB default 16S dna prep"
     prep.ncbi_taxon_id = "408170" # human gut metagenomes
     prep.lib_layout = "fragment"
@@ -52,13 +61,28 @@ seqtype_docs = {
     "trimmed": ("Trimmed 16S","Trimmed ")
 }
 
+def seq_len_mode(fname, sample_size=200):
+    """Return the most common length of sequences from a given sequence
+    file"""
+
+    seq_sample = islice(
+        SeqIO.parse(fname, anadama.util.guess_seq_filetype(fname)),
+        None, sample_size
+    )
+    return max(countby(len, seq_sample).iteritems(), key=second)[0]
+    
+
 def parse_record(r, sample, fname, md5sum, seqtype="raw"):
     prep = default_prep(sample)
     com, doc = seqtype_docs[seqtype]
-    seq = cutlass.SixteenSRawSeqSet()
+    maybe_seqs = list(prep.raw_seq_sets())
+    if maybe_seqs:
+        seq = maybe_seqs[0]
+    else:
+        seq = cutlass.SixteenSRawSeqSet()
     seq.comment = com+str(sample)
     seq.checksums = {"md5": md5sum}
-    seq.exp_length = 175
+    seq.exp_length = seq_len_mode(fname)
     seq.format = anadama.util.guess_seq_filetype(fname)
     seq.format_doc = doc+seq.format
     seq.local_file = fname
@@ -88,6 +112,12 @@ def find_fname(substr, fnames, commonprefix):
     if not matches:
         return
     return commonprefix+matches[0]
+
+def _getter(idx_or_callable):
+    if callable(idx_or_callable):
+        return idx_or_callable
+    else:
+        return itemgetter(idx_or_callable)
     
 def from_file(fname, samples, seq_fnames, sample_id_idx=3,
               fname_idx=0, nest=True, n_procs=8, parse_record=parse_record):
@@ -96,11 +126,13 @@ def from_file(fname, samples, seq_fnames, sample_id_idx=3,
     md5s = pool.map(md5, seq_fnames)
     md5s = dict([(f,m) for f,m in zip(seq_fnames, md5s)])
     commonprefix, fnames = rm_common_prefix(seq_fnames)
+    _sample_id = _getter(sample_id_idx)
+    _fname = _getter(fname_idx)
     for record in fields(fname):
-        sampleid = record[sample_id_idx]
+        sampleid = _sample_id(record)
         if sampleid not in samples:
             continue
-        seq_fname = find_fname(record[fname_idx], fnames, commonprefix)
+        seq_fname = find_fname(_fname(record), fnames, commonprefix)
         if not seq_fname:
             continue
         ret = parse_record(record, samples.get(sampleid)[0],
