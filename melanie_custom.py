99b0cc24c2d9076c4d521ce4eaf3671815f6fc58
rschwager-hsph <schwager@hsph.harvard.edu>
2016-11-19 13:58:38 -0500

    add melanie's requested changes

--- melanie_custom.py
+++ melanie_custom.py
@@ -6,22 +6,31 @@ from all_pipeline import (
     h2ntdb,
     h2pdb,
     kneaddb_deps,
+    kneaddb,
     conf,
     bin
     )
 from anadama import RunContext
+from anadama.backends import LevelDBBackend
 from anadama.reporters import LoggerReporter
 from anadama.sge import SGEPowerup
 from anadama.deps import HugeFileDependency as HFD
+from anadama.deps import GlobDependency
 from anadama.util import fname
+
+be = LevelDBBackend("melanie_db", False)
 
 dogdir = "/broad/geverslab_microbiome/melanie/HMP2_March2016/data/TechRep_DogStool/bam"
 inbams = glob(dogdir+"/*.bam")
 
-ctx = RunContext(grid_powerup=SGEPowerup(queue="long", tmpdir="/seq/ibdmdb/tmp/anasge"))
+ctx = RunContext(storage_backend=be,
+                 grid_powerup=SGEPowerup(queue="long", tmpdir="/seq/ibdmdb/tmp/anasge"))
 reporter = LoggerReporter(ctx, loglevel_str='debug', 
                           logfile="/seq/ibdmdb/melanie_run.log")
 outdir = "/seq/ibdmdb/melanie"
+dogdb = "/seq/ibdmdb/centos6/var/lib/CanFam3.1"
+dogdb_dep = GlobDependency(dogdb+"/*")
+ctx.already_exists(dogdb_dep)
 ctx.already_exists(*bin.values())
 ctx.already_exists(h2ntdb, h2pdb)
 ctx.already_exists(*kneaddb_deps.values())
@@ -32,12 +41,14 @@ for bam in inbams:
     ctx.already_exists(bamdep)
     cleanfq = fname.mangle(bam, dir=outdir, ext="fastq")
     knead(ctx, bamdep, cleanfq,
-          fname.mangle(bam, dir=outdir, tag="clean", ext="log"))
+          fname.mangle(bam, dir=outdir, tag="clean", ext="log"),
+          dbs=[dogdb, kneaddb.six], db_deps=[dogdb_dep, kneaddb_deps.six])
     wgs_taxtxt = fname.mangle(bam, dir=outdir, tag="tax", ext="txt")
     metaphlan2(ctx, cleanfq, 
                fname.mangle(bam, dir=outdir, ext="biom"),
                wgs_taxtxt, 
-               fname.mangle(bam, dir=outdir, tag="strain", ext="bam"))
+               fname.mangle(bam, dir=outdir, tag="strain", ext="bam"),
+               mem=3500, time=120)
     humann2(ctx, cleanfq, wgs_taxtxt, 
             fname.mangle(bam, dir=outdir, tag="humann2", ext="tar.bz2"))
 
