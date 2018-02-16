54e9b7128cbcfa10c6b081e4cf1093407dda0c3a
Cesar Arze <cesar.arze@gmail.com>
2018-02-16 11:06:56 -0500

    Adding more fleshed out MTX viz pipeline and first go at 16S viz pipeline.

---
+++ hmp2_workflows/workflows/16s_viz.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+
+"""
+hmp2_workflows.workflows.16s_vis
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+An AnADAMA2 workflow that handles visualzation of output from the HMP2 
+16S metagenome pipeline. 
+
+This workflow generates a static HTML summary page that summarizes the
+results for easier consumption
+
+Copyright (c) 2018 Harvard School of Public Health
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
+
+import os
+
+from glob2 import glob
+
+from anadama2 import Workflow
+
+from hmp2_workflows import document_templates
+from biobakery_workflows import utilities, files
+
+
+def parse_cli_arguments():
+    """Parses any command-line arguments passed into the workflow.
+
+    Args:
+        None
+    Requires:
+        None
+    Returns:
+        anadama2.Workflow: The workflow object for this pipeline
+        anadama2.cli.Configuration: Arguments passed into this workflow.
+    """
+    workflow = Workflow(version='1.0', description='A workflow to handle visualization '
+                        'of HMP2 16S data.')
+    workflow.add_argument('config-file', desc='Configuration file '
+                          'containing parameters required by the workflow.')
+    workflow.add_argument('metadata-file', desc='Accompanying metadata file '
+                           'for the provided data files.', default=None)
+
+
+    return workflow
+
+
+def main(workflow):
+    args = workflow.parse_args()
+
+    otu_table = glob(os.path.join(args.input + '/**/', 
+        files.SixteenS.file_info['otu_table_closed_reference'].keywords.get('names')))[0]
+    otu_table_open = glob(os.path.join(args.input + '/**/',
+        files.SixteenS.file_info['otu_table_open_reference'].keywords.get('names')))[0]
+    read_counts_table = glob(os.path.join(args.input + "/**/",
+        files.SixteenS.file_info['read_count_table'].keywords.get('names')))[0]
+    eestats_table = glob(os.path.join(args.input + "/**/",
+        files.SixteenS.file_info['eestats2'].keywords.get('names')))[0]
+    centroid_fasta = glob(os.path.join(args.input + "/**/",
+        files.SixteenS.file_info['msa_nonchimera'].keywords.get('names')))[0]
+    centroid_closed_fasta = glob(os.path.join(args.input + "/**/",
+        files.SixteenS.file_info['msa_closed_reference'].keywords.get('names')))[0]
+    centroid_closed_fasta = glob(os.path.join(args.input + "/**/",
+        files.SixteenS.file_info['msa_closed_reference'].keywords.get('names')))[0]
+    log_file = files.Workflow.path('log', args.input)
+
+    dependencies = [otu_table, otu_table_open, read_counts_table, eestats_table,
+                    centroid_fasta, centroid_closed_fasta, log_file]
+
+    templates = []
+    templates.append(document_templates.get_template('header'))
+    templates.append(document_templates.get_template('16s'))
+    templates.append(document_templates.get_template('footer'))
+
+    doc_task = workflow.add_document(
+        templates = templates,
+        depends = dependencies,
+        targets = workflow.name_output_files("summary.html"),
+        vars = {
+            'summary_title': "HMP2: 16S Data Summary Report",
+            "otu_table": otu_table,
+            "read_count_table": read_counts_table,
+            "eestats_table" :eestats_table,
+            "format": args.format,
+            "log": log_file
+        },
+        table_of_contents = False
+    )
+
+    workflow.add_task("sed -i -e 's/ xmlns=\"http:\/\/www.w3.org\/1999\/xhtml\" lang=\"\" xml:lang=\"\"//' [depends[0]]",
+                      depends = workflow.name_output_files('summary.html'),
+                      targets = workflow.name_output_files('summary.html'))
+
+    workflow.add_task("sed -i -e '/<header>/,/<\/header>/d' [depends[0]]",
+                      depends = workflow.name_output_files('summary.html'),
+                      targets = workflow.name_output_files('summary.html'))
+
+    workflow.add_task("sed -i -e '/img.* alt/! s/img/img alt=\"\"/' [depends[0]];",
+                      depends = workflow.name_output_files('summary.html'),
+                      targets = workflow.name_output_files('summary.html'))
+
+    workflow.add_task("rm [depends[0]]",
+                      depends = workflow.name_output_files('summary.html-e'))
+
+    workflow.add_task('tidy -q -i --wrap 0 -m [depends[0]]',
+                      depends = workflow.name_output_files('summary.html'),
+                      targets = workflow.name_output_files('summary.html'))
+
+    workflow.go()
+
+
+if __name__ == "__main__":
+    main(parse_cli_arguments())
